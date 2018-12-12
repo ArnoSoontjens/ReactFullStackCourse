7adc825938e44b5bc0da614c13d71635c834b622
Arno Soontjens <arnosoontjens@outlook.com>
2018-12-12 13:12:32 +0100

    Added auth with google OAuth2 and added route handlers to handle login. Moved passport config to separate module

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const config = require("config");
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+require("./auth/passport")();
+
+const login = require("./routes/auth");
+
+app.use("/auth", login);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ auth/passport.js
@@ -0,0 +1,20 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const config = require("config");
+
+module.exports = () => {
+  return passport.use(
+    new GoogleStrategy(
+      {
+        clientID: config.get("Auth.Google.clientId"),
+        clientSecret: config.get("Auth.Google.clientSecret"),
+        callbackURL: "/auth/google/callback"
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log("Access token: ", accessToken);
+        console.log("Refresh token: ", refreshToken);
+        console.log("Profile: ", profile);
+      }
+    )
+  );
+};
